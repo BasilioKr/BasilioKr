8d0bf73ebb32f7da736121e25f79081ad3d9859a
BasilioKr <basilio.dev1@gmail.com>
2022-10-23 23:08:14 +0300

    css layout & defoult

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@
   	------------------------------------------------------ */
   	setTimeout(function() {
 
-   	$('h1.responsive-headline').fitText(1.2, { minFontSize: '25px', maxFontSize: '40px' });
+   	$('h1.responsive-headline').fitText(1.2, { minFontSize: '15px', maxFontSize: '40px' });
 
   	}, 100);
 
